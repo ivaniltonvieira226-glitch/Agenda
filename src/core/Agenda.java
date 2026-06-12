b231141726c21cc353ec047d9a3ba91b90180d65
SJeanF <fonsecasantosjean@gmail.com>
2026-06-11 21:36:33 -0300

    fix: validar se quando atualizar a tarefa atual se tem uma tarefa depois da atual

--- src/core/Agenda.java
+++ src/core/Agenda.java
@@ -52,7 +52,9 @@ public class Agenda {
 
     public void atualizarTarefaAtual(StatusTarefa status) {
         this.tarefaAtual.setStatus(status);
-        if (status != StatusTarefa.Pendente) this.tarefaAtual = this.tarefaAtual.proxTarefa;
+        if (status != StatusTarefa.Pendente && tarefaAtual.proxTarefa != null) {
+            this.tarefaAtual = this.tarefaAtual.proxTarefa;
+        }
     }
 
     private boolean adicionarALista(Tarefa novaTarefa) {
